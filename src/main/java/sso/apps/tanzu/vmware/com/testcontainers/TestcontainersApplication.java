566d2c3ef72a0a864ee25f151ab57af83f065716
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-12-05 16:26:43 +0100

    initial commit

---
+++ src/main/java/sso/apps/tanzu/vmware/com/testcontainers/TestcontainersApplication.java
@@ -0,0 +1,13 @@
+package sso.apps.tanzu.vmware.com.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersApplication.class, args);
+	}
+
+}
